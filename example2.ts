df6ca53aa770c2e72931a1bff2afd61167f6046e
yourgirlcodes <zoecohen01@gmail.com>
2020-09-23 15:15:31 +0300

    application to PIN

--- example2.ts
+++ example2.ts
@@ -25,41 +25,43 @@
 // you have to declare what properties you are going to apply to the class 
 // normal ES6 - you just declare properties on the fly
 
-// class Farm {
-//     id           // telling you that we have an id on this class - we dont need this in ES6, but we do in TS
-//     constructor() {
-//         this.id = 'corn'
-//     }
-// }
+class Farm {
+    id: string           
+    // telling you that we have an id on this class and it has a type string
+    // we don't need this (id) in ES6, but we do in TS
+    constructor() {
+        this.id = 'corn'
+    }
+}
 
-// // then 
-// function showFarmCode(ComponentClass) {
+// then:
+function showFarmCode(ComponentClass) {
+    const comp = new ComponentClass() // NEED TO SET AN INSTANCE!!
+    var farms = document.getElementById('farm-list') // if there is this id
+    if (farms) {
+        farms.innerHTML = generateFarmCodes({
+            name: comp.id,
+            length: 7
+        })
+    }
+}
 
-//     var farms = document.getElementById(ComponentClass.id)
-//     if (farms) {
-//         farms.innerHTML = generateFarmCodes({ 
-//             name: ComponentClass.id,
-//             length: 4
-//         })
-//     }
-// }
 
+type ValidFarms = 'corn' | 'cattle'
 
-//  type ValidFarms = 'corn' | 'cattle'
+interface GenerateFarmConfigs {
+    name: ValidFarms,
+    length: number
+}
 
-//  interface GenerateConfigs {
-//     name: ValidFarms,
-//     length: number
-// }
+function generateFarmCodes(name: ValidFarms, length: number): string // two params
+function generateFarmCodes(options: GenerateFarmConfigs): string // single object
+function generateFarmCodes(optionsOrFarm: GenerateFarmConfigs | ValidFarms): string {
+    if (typeof optionsOrFarm === 'string') {
+        return optionsOrFarm + ":" + " " + Math.random().toString(36).substr(2, length) // we haven't yet defined length
 
-//  function generateFarmCodes(name: ValidFarms, length: number): string // two params
-//  function generateFarmCodes(options: GenerateConfigs): string // single object
-//  function generateFarmCodes(optionsOrFarm: GenerateConfigs | ValidFarms): string {
-//     if (typeof optionsOrFarm === 'string') {
-//         return optionsOrFarm + ":" + Math.random().toString(36).substr(2, length) // we haven't yet defined length
+    }
+    return optionsOrFarm.name + ":" + " " + Math.random().toString(36).substr(2, optionsOrFarm.length)
+}
 
-//     }
-//     return optionsOrFarm.name + ":" + Math.random().toString(36).substr(2, optionsOrFarm.length)
-// }
-
-// showFarmCode(Farm)
+showFarmCode(Farm)
--- src/app.ts
+++ src/app.ts
@@ -1,66 +1,28 @@
-// import { generateRandomPin } from './utils'
+import { generateRandomPin } from './utils'
 
-// class App{
-//     id
-//     constructor(){
-//         this.id="app"
-//     }
-// }
-
-// function main() {
-//     var appComponent = document.getElementById('app');
-//     // note that this can actually be null 
-//     // (null is a type in newer angular versions)
-//     // opt-in strictNullChecks = true in config if you want to have the check for null
-//     // if you do opt-in then include a check for if(app) INSIDE of the setInterval
-//     // BEST PRACTICE: OPT-IN --> powerful 
-//     setInterval(function () {
-//         if (appComponent) {
-//             appComponent.innerHTML = generateRandomPin({ letter: 'A', length: 7 });
-//         }
-//     }, 1000);
-// }
-
-// main();
-
-
-class Farm {
-    id: string           
-    // telling you that we have an id on this class and it has a type string
-    //- we dont need this (id) in ES6, but we do in TS
-    constructor() {
-        this.id = 'corn'
+class App{
+    id: string
+    constructor(){
+        this.id="app"
     }
 }
 
-// then:
-function showFarmCode(ComponentClass) {
-    const comp = new ComponentClass() // NEED TO SET AN INSTANCE!!
-    var farms = document.getElementById('app')
-    if (farms) {
-        farms.innerHTML = generateFarmCodes({
-            name: comp.id,
-            length: 7
-        })
-    }
+function main(ClassComponent) {
+    const comp = new ClassComponent()
+    
+    var appComponent = document.getElementById(comp.id);
+    // note that this can actually be null 
+    // (null is a type in newer angular versions)
+    // opt-in strictNullChecks = true in config if you want to have the check for null
+    // if you do opt-in then include a check for if(app) INSIDE of the setInterval
+    // BEST PRACTICE: OPT-IN --> powerful 
+    setInterval(function () {
+        if (appComponent) {
+            appComponent.innerHTML = "Your PIN will change every 2 seconds" + "<br />" + "PIN: " + generateRandomPin({ letter: 'A', length: 4 });
+        }
+    }, 2000);
 }
 
+main(App);
 
-type ValidFarms = 'corn' | 'cattle'
 
-interface GenerateConfigs {
-    name: ValidFarms,
-    length: number
-}
-
-function generateFarmCodes(name: ValidFarms, length: number): string // two params
-function generateFarmCodes(options: GenerateConfigs): string // single object
-function generateFarmCodes(optionsOrFarm: GenerateConfigs | ValidFarms): string {
-    if (typeof optionsOrFarm === 'string') {
-        return optionsOrFarm + ":" + " " + Math.random().toString(36).substr(2, length) // we haven't yet defined length
-
-    }
-    return optionsOrFarm.name + ":" + " " + Math.random().toString(36).substr(2, optionsOrFarm.length)
-}
-
-showFarmCode(Farm)
